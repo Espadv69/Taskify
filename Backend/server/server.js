800b356021be9793a25efe0b34a93f8539e3bdd1
Espadv69 <aitorjob69@gmail.com>
2025-02-10 19:18:48 +0100

    Feat: load environment variables and initialize Express

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -3,3 +3,9 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const morgan = require('morgan')
+
+// Carga variable de entorno
+dotenv.config()
+
+// Inicializar con express
+const app = express()
