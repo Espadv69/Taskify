b3c53ad708fea468fce89817154dab682848c2f8
Espadv69 <aitorjob69@gmail.com>
2025-02-10 19:35:22 +0100

    Feat: define User schema with name, email, and password

--- Backend/server/models/User.js
+++ Backend/server/models/User.js
@@ -1,2 +1,24 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+
+// Definir el esquema del usuario
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Agrega automáticamente createdAt y updatedAt
+  }
+)
