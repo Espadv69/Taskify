40332e4662056dc4ecc5d4968be5182e812207d3
Espadv69 <aitorjob69@gmail.com>
2025-02-10 19:23:29 +0100

    Feat: connect to MongoDB using mongoose

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -14,3 +14,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch((error) => console.error('❌ MongoDB connection error:', error))
