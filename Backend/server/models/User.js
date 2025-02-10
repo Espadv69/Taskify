38982a77b8fcca2fc984c2276a5341fc9d79d6fc
Espadv69 <aitorjob69@gmail.com>
2025-02-10 19:39:29 +0100

    Middleware: Hash password before saving user

--- Backend/server/models/User.js
+++ Backend/server/models/User.js
@@ -22,3 +22,12 @@ const UserSchema = new mongoose.Schema(
     timestamps: true, // Agrega automáticamente createdAt y updatedAt
   }
 )
+
+// Middleware: Hashear la contraseña antes de guardar el usuario
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
