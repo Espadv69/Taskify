640afbf802da60e9f30c9abfa723784ffb9e2192
Espadv69 <aitorjob69@gmail.com>
2025-02-11 17:32:25 +0100

    Refactor

--- Backend/server/models/User.js
+++ Backend/server/models/User.js
@@ -1,36 +1,9 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose');
 
-// Definir el esquema del usuario
-const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true, // Agrega automáticamente createdAt y updatedAt
-  }
-)
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
 
-// Middleware: Hashear la contraseña antes de guardar el usuario
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
-
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-  next()
-})
-
-// Exportar el modelo
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', userSchema);
