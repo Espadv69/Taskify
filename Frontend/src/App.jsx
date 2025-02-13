7b371056c55b8a30e1243ae3aef0e3ee4759c3b8
Espadv69 <aitorjob69@gmail.com>
2025-02-13 17:08:24 +0100

    Add hooks tasks

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,9 @@
-const App = () => {}
+import { useState } from 'react'
+
+const App = () => {
+  const [tasks, setTasks] = useState([])
+  const [taskName, setTaskName] = useState('')
+  const [taskToDo, setTaskToDo] = useState('')
+}
 
 export default App
