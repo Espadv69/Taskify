1ca7139908d966ce0943f36fdfa0b65d5743c543
Espadv69 <aitorjob69@gmail.com>
2025-03-18 12:31:46 +0100

    Add endpoint to delete a task by ID

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -51,6 +51,23 @@ app.get('/tasks', async (req, res) => {
   }
 })
 
+// Endpoint para borra una tarea por su ID
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const task = await Task.findByIdAndDelete(id)
+
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' })
+    }
+
+    res.status(200).json(task)
+  } catch (err) {
+    console.error('Error deleting task:', err)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
 const server = app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
 )
